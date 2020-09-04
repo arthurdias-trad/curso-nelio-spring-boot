932327160359108a4cfc544a87cf2471433e4ecc
Arthur Dias <arthurdias.trad@outlook.com>
2020-09-04 10:40:47 -0300

    Projeto criado

---
+++ src/test/java/br/com/arthurdias/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arthurdias.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
