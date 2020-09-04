3f8b71f7c09fd501aec7565e75e2fd7d41254b0c
Arthur Dias <arthurdias.trad@outlook.com>
2020-09-04 13:19:19 -0300

    Criados repository e service para Categoria

---
+++ src/main/java/br/com/arthurdias/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package br.com.arthurdias.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.arthurdias.cursomc.domain.Categoria;
+import br.com.arthurdias.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
