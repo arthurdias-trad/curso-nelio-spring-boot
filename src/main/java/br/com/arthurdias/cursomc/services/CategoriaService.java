319f0634a4a536588f89bcf4dde6ca4464b5a3ea
Arthur Dias <arthurdias.trad@outlook.com>
2020-10-14 13:49:33 -0300

    Endpoint para excluir Categoria via DELETE disponibilizado com tratamento de erro de integridade de dados

--- src/main/java/br/com/arthurdias/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/arthurdias/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package br.com.arthurdias.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.arthurdias.cursomc.domain.Categoria;
 import br.com.arthurdias.cursomc.repositories.CategoriaRepository;
+import br.com.arthurdias.cursomc.services.exceptions.DataIntegrityException;
 import br.com.arthurdias.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,5 +33,15 @@ public class CategoriaService {
 		this.find(obj.getId());
 		
 		return this.repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		this.find(id);
+		
+		try {
+			this.repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que tem produtos cadastrados");
+		}
 	}
 }
